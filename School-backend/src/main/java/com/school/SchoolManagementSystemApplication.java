bca750da6686c404b87c737ec87631588ff4111e
Abuo Bernard <abuobernard@gmail.com>
2026-05-31 07:23:34 +0000

    modification

--- School-backend/src/main/java/com/school/SchoolManagementSystemApplication.java
+++ School-backend/src/main/java/com/school/SchoolManagementSystemApplication.java
@@ -2,11 +2,17 @@ package com.school;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv; // Import the Dotenv class
 
 @SpringBootApplication
 public class SchoolManagementSystemApplication { // <--- Add the 's' here!
 
     public static void main(String[] args) {
+
+        Dotenv dotenv = Dotenv.configure().load(); // Load environment variables from .env file
+        System.setProperty("url", dotenv.get("url")); // Set the 'url'
+        System.setProperty("username", dotenv.get("username")); // Set the 'username'
+        System.setProperty("password", dotenv.get("password")); // Set the 'password'
         SpringApplication.run(SchoolManagementSystemApplication.class, args); // <--- And here!
     }
 }
